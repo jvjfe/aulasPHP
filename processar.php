cd892edca9d4a46138ae524b2bb1a4d2aaa5f0b7
Jvjfe <82617361+jvjfe@users.noreply.github.com>
2024-08-07 21:48:34 -0300

    Add files via upload
    Finalizando a atualização do código em POO

--- processar.php
+++ processar.php
@@ -2,11 +2,25 @@
     $nome = $_GET["campoNome"];
     $nota1 = $_GET["campoNota1"];
     $nota2 = $_GET["campoNota2"];
+
     //acessando arquivos do classeBoletim.php
+
     require("classeBoletim.php");
-    //
+
+    // Instanciamos a partir da classe Boletim
+
     $boletim = new Boletim(); //construção do método construtor Boletim()
-    $boletim->setNome($nome)
+    $boletim->setNome($nome); // setando valores do nome
+    $boletim->setNota1($nota1); //setando valores da nota1
+    $boletim->setNota2($nota2); // setando valores da nota2
+    $boletim->calcularMedia(); //executando o método calcularMedia
+    $boletim->calcularResultado(); //executando o método calcularResultado
+
+    // obtendo os valores dos atributos (GET) e armazenando em variáveis
+    
+    $media = $boletim->getMedia();
+    $resultado = $boletim->getResultado();
+
     //Criar e declarar tem o mesmo significado, porém criamos variaveis e instanciamos objetos
 
 ?>
